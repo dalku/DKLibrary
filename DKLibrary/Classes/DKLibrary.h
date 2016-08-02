adc88f8361e50f033ed1b26f19656a024b7927e6
Dalku-iMac <dalku-imac@Dalku-iMacui-iMac.local>
2016-08-02 14:10:20 +0900

    11

---
+++ DKLibrary/Classes/DKLibrary.h
@@ -0,0 +1,13 @@
+//
+//  DKLibrary.h
+//  Pods
+//
+//  Created by Dalku-iMac on 2016. 8. 2..
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DKLibrary : NSObject
+-(void)helloDalku;
+@end
---
+++ DKLibrary/Classes/DKLibrary.m
@@ -0,0 +1,17 @@
+//
+//  DKLibrary.m
+//  Pods
+//
+//  Created by Dalku-iMac on 2016. 8. 2..
+//
+//
+
+#import "DKLibrary.h"
+
+@implementation DKLibrary
+
+-(void)helloDalku
+{
+    NSLog(@"### Hello Dalku");
+}
+@end
